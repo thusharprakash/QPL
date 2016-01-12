6474d5c7ff765d961930d4ced3df33f660c7001e
aswinkk1 <aswinkk1@gmail.com>
2016-01-12 16:49:36 +0530

    Added ngdialog box

--- QPLeague/qpl/static/qpl/js/qplCtrl.js
+++ QPLeague/qpl/static/qpl/js/qplCtrl.js
@@ -1,5 +1,19 @@
-var app = angular.module('qpl',[]);
+var app = angular.module('qpl',['ngDialog']);
 
-app.controller('qplController', function() {
-	console.log("dsada");
+app.controller('qplController', function($scope,ngDialog,$compile) {
+	$scope.openTeam = function(teamName){
+		console.log(teamName);
+		ngDialog.open({
+			template: 'teams/'+teamName,
+			closeByDocument: false,
+            className: 'ngdialog-theme-default',
+            closeByEscape: true,
+            showClose: true,
+            scope: $scope
+
+		});
+	};
+	window.onload = function(){
+		$compile(document.getElementsByTagName('body')[0])($scope);
+	};
 });
